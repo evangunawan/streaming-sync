1d68a515628ddd555fce91a120e48ace4b67b150
motionfrog <evangunawan14@gmail.com>
2019-12-04 19:31:25 +0700

    first commit

---
+++ src/Counter.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default class Counter extends React.Component {
+  
+  render () {
+    return (
+      <div>
+        <h2>Hello World</h2>
+      </div>
+    );
+  }
+}
